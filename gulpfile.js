c73a6643a7d0988207952ac93a16ce31a4c3b701
EminShahbazi <emin.shahbazi@outlook.com>
2025-10-05 18:15:55 +0200

    compile and minify sass files with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,36 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer").default;
+const cleanCss = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const sourcemaps = require("gulp-sourcemaps");
 
-function html() {
+function html(done) {
   src("src/*.html").pipe(dest("dist"));
+
+  done();
+}
+
+function styles(done) {
+  src("src/styles/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCss())
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest("dist/css"));
+
+  done();
 }
 
 function watchFiles() {
   watch("src/*.html", html);
+  watch("src/styles/**/*.scss", styles);
 }
 
-exports.default = watchFiles;
+exports.default = series(html, styles, watchFiles);
